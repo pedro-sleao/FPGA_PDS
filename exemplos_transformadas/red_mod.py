049b54c8a4ad80eeb01e11f70e72886e3c8e46e9
Pedro Lucas <pedroca242@gmail.com>
2025-05-27 13:39:39 -0300

    Função para redução modular de Montgomery

--- exemplos_transformadas/red_mod.py
+++ exemplos_transformadas/red_mod.py
@@ -1,22 +1,28 @@
 import math
 
 q = 3329
+k = math.ceil(math.log2(q))
+r = 2**k
 
 # Montgomery reduction parameters
-R = 2**16
-qinv = -pow(q, -1, R) % R
+qinv = -pow(q, -1, r) % r
 
 # Barret reduction parameters
-k = math.ceil(math.log2(q))
-mu = math.floor(2**(2*k)/q)
+mu = math.floor(r**(2)/q)
 
 def barret_red_3329(a):
     t = (a * mu) >> (2 * k)
-    r = a - t * q
-    if r >= q:
-        r -= q
-    elif r < 0:
-        r += q
-    return r
+    c = a - t * q
+    if c >= q:
+        c -= q
+    elif c < 0:
+        c += q
+    return c
 
-print(qinv)
+def montgomery_red_3329(a):
+    am = (a * r) % q    
+    t = (am * qinv) % r
+    cm = (am + t * q) >> k
+    return cm
+
+print(montgomery_red_3329(3330))
