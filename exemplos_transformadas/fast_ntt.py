b38da622228af4d9e03936a1a1f729d50ea797ab
pedro <pedroca242@gmail.com>
2025-05-29 14:34:15 -0300

    Função para a NTT utilizando o CT e montgomery reduction

--- exemplos_transformadas/fast_ntt.py
+++ exemplos_transformadas/fast_ntt.py
@@ -1,9 +1,8 @@
-import sage.all as sage
+from red_mod import kred_3329, barret_red_3329, montgomery_red_3329
 
 n = 256
 q = 3329
-Zq = sage.Integers(q)
-primitive_root = min(Zq.zeta(n, all=True))
+primitive_root = 17
 
 def bit_reverse_copy(a, n):
     result = [0]*n
@@ -27,45 +26,18 @@ def ct_ntt_256_3329(a: list[int]) -> list[int]:
 
     m = 1   
     while m < n:
-        w_m = primitive_root ** (n // (2*m))
+        w_m = pow(primitive_root, (n // (2*m)), q)
         for i in range(0, n, 2*m):
-            w = Zq(1)
+            w = 1
             for j in range(m):
                 u = A[i + j]
-                v = Zq(w * A[i + j + m])
-                A[i + j] = Zq(u + v)
-                A[i + j + m] = Zq(u - v)
-                w *= Zq(w_m)
+                v = montgomery_red_3329(w * A[i + j + m])
+                A[i + j] = montgomery_red_3329(u + v)
+                A[i + j + m] = montgomery_red_3329(u - v)
+                w *= w_m
         m <<= 1
 
     return A
-
-def gs_intt_256_3329(A: list[int]) -> list[int]:
-    if len(A) != n:
-        raise ValueError()
-    if not all((0 <= int(val) < q) for val in A):
-        raise ValueError()
-
-    a = A[:]
-
-    m = n//2   
-    while m > 0:
-        w_m = primitive_root ** (-n // (2*m))
-        for i in range(0, n, 2*m):
-            w = Zq(1)
-            for j in range(m):
-                u = a[i + j]
-                v = a[i + j + m]
-                a[i + j] = Zq(u + v)
-                a[i + j + m] = Zq((u - v) * w)
-                w *= Zq(w_m)
-        m >>= 1
-    
-    a = [i*sage.inverse_mod(n, q) for i in a]
-
-    a = bit_reverse_copy(a, n)
-
-    return a
 
 a = [i for i in range(n)]
 A = ct_ntt_256_3329(a)
---
+++ exemplos_transformadas/fast_ntt_sage.py
@@ -0,0 +1,73 @@
+import sage.all as sage
+
+n = 256
+q = 3329
+Zq = sage.Integers(q)
+primitive_root = min(Zq.zeta(n, all=True))
+
+def bit_reverse_copy(a, n):
+    result = [0]*n
+    bits = n.bit_length()-1
+    for k in range(n):
+        rev = 0
+        x = k
+        for _ in range(bits):
+            rev = (rev << 1) | (x & 1)
+            x >>= 1
+        result[rev] = a[k]
+    return result
+
+def ct_ntt_256_3329(a: list[int]) -> list[int]:
+    if len(a) != n:
+        raise ValueError()
+    if not all((0 <= int(val) < q) for val in a):
+        raise ValueError()
+
+    A = bit_reverse_copy(a, n)
+
+    m = 1   
+    while m < n:
+        w_m = primitive_root ** (n // (2*m))
+        for i in range(0, n, 2*m):
+            w = Zq(1)
+            for j in range(m):
+                u = A[i + j]
+                v = Zq(w * A[i + j + m])
+                A[i + j] = Zq(u + v)
+                A[i + j + m] = Zq(u - v)
+                w *= Zq(w_m)
+        m <<= 1
+
+    return A
+
+def gs_intt_256_3329(A: list[int]) -> list[int]:
+    if len(A) != n:
+        raise ValueError()
+    if not all((0 <= int(val) < q) for val in A):
+        raise ValueError()
+
+    a = A[:]
+
+    m = n//2   
+    while m > 0:
+        w_m = primitive_root ** (-n // (2*m))
+        for i in range(0, n, 2*m):
+            w = Zq(1)
+            for j in range(m):
+                u = a[i + j]
+                v = a[i + j + m]
+                a[i + j] = Zq(u + v)
+                a[i + j + m] = Zq((u - v) * w)
+                w *= Zq(w_m)
+        m >>= 1
+    
+    a = [i*sage.inverse_mod(n, q) for i in a]
+
+    a = bit_reverse_copy(a, n)
+
+    return a
+
+a = [i for i in range(n)]
+A = ct_ntt_256_3329(a)
+
+print(A)
